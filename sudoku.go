667d09a4374301b9e48ebb425e0404211e0f77e5
Andrey <dron19812013@gmail.com>
2025-02-18 14:41:07 +0300

    3

--- sudoku.go
+++ sudoku.go
@@ -17,21 +17,59 @@ var (
 )
 
 type cell struct {
-	number int
-	x, y   int
-	fits   bool
+	x, y, number int
+	fix          bool
 }
 
-type greed [height][width]int
+type greed [height][width]cell
 
-func (g greed) cellDigitChecking(c cell) bool {
+func newSudoku(greed1 [width][height]int) *greed { //Заполнение поля сверху вниз, слева на право массивом greed1
+	var greed2 greed
+	for i := 1; i < width; i++ {
+		for j := 1; j < height; j++ {
+			a := greed1[i][j]
+			if a != 0 {
+				greed2[i][j].number = a
+				// greed2[i][j].fix = true
+			}
+		}
+	}
+	return &greed2
+}
+
+func fieldInitial() *greed { // Заполнение поля вручную массивом инициализированным композитным литералом
+
+	return newSudoku([width][height]int{
+		{5, 3, 0, 0, 7, 0, 0, 0, 0},
+		{6, 0, 0, 1, 9, 5, 0, 0, 0},
+		{0, 9, 8, 0, 0, 0, 0, 6, 0},
+		{8, 0, 0, 0, 6, 0, 0, 0, 3},
+		{4, 0, 0, 8, 0, 3, 0, 0, 1},
+		{7, 0, 0, 0, 2, 0, 0, 0, 6},
+		{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		{0, 0, 0, 4, 1, 9, 0, 0, 5},
+		{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	})
+}
+
+func fixedField(g *greed) { //Делает все значения не равные нулю неизменяемыми
+	for i := 1; i < width; i++ {
+		for j := 1; j < height; j++ {
+			if g[i][j].number != 0 {
+				g[i][j].fix = true
+			}
+		}
+	}
+}
+
+func (g *greed) cellDigitChecking(c cell) bool { //Проверка квадрата, в котором ставится цифра на отсутствие совпадений
 	var a, b int
 	var d bool
 	a = c.x / 3
 	b = c.y / 3
 	for i := 1; i <= 3; i++ {
-		for j := 1; i <= 3; i++ {
-			if c.number != g[a*3+i][b*3+j] {
+		for j := 1; j <= 3; j++ {
+			if c.number != g[a*3+i][b*3+j].number && c.x != i && c.y != j {
 				d = true
 			} else {
 				Println(ErrDigitSquare)
@@ -42,10 +80,10 @@ func (g greed) cellDigitChecking(c cell) bool {
 	return d
 }
 
-func (g greed) cellLineChecking(c cell) bool {
+func (g *greed) cellLineChecking(c cell) bool { //Проверка гор линии, в которую ставится цифра, на несовпадение
 	var d bool
 	for i := 0; i < width; i++ {
-		if c.number != g[i][c.y] {
+		if c.number != g[i][c.y].number {
 			d = true
 		} else {
 			d = false
@@ -55,10 +93,10 @@ func (g greed) cellLineChecking(c cell) bool {
 	return d
 }
 
-func (g greed) cellColumnChecking(c cell) bool {
+func (g *greed) cellColumnChecking(c cell) bool { //Проверка вертикальной линии, в которой ставится цифра, на несовппадение
 	var d bool
 	for i := 0; i < height; i++ {
-		if c.number != g[c.x][i] {
+		if c.number != g[c.x][i].number {
 			d = true
 		} else {
 			d = false
@@ -66,6 +104,21 @@ func (g greed) cellColumnChecking(c cell) bool {
 		}
 	}
 	return d
+}
+
+func (g *greed) cellNotFix(c cell) bool {
+	return g[c.x][c.y].fix
+}
+
+func (g *greed) addDigit(c cell) {
+	if g.cellDigitChecking(c) && g.cellLineChecking(c) && g.cellColumnChecking(c) && g.cellNotFix(c) {
+		g[c.x][c.y].number = c.number
+	}
+
+}
+
+func main() {
+	Print(fixedField(fieldInitial()))
 }
 
 // func(c cell) cellFiling(i, j int, g greed) {
